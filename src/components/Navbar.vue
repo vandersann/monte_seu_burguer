6cc7ac2b66b17fea40edbd18d469c73286c33dd8
Vander Sandino <65673565+vandersann@users.noreply.github.com>
2023-07-17 18:26:45 -0300

    Inicio do projeto

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav>
+        <router-link to="/">
+            <img src="" id="logo">
+        </router-link>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/pedidos">Pedidos</router-link>
+    </nav>
+</template>
+<script>
+export default {
+    name: "Navbar"
+}
+</script>
+<style scoped>
+
+</style>
